62ecf756ff4924c1c9e1bd6b0a186d752d63ad65
jacksonmed <ian@jacksonmed.org>
2021-06-10 15:36:44 -0700

    CLI interface along with basic functionality completed

---
+++ file/file_transfer.py
@@ -0,0 +1,31 @@
+import paramiko
+import scp
+from scp import SCPClient
+import yaml
+
+stream = open("config.yaml", 'r')
+configuration = yaml.load(stream)
+
+
+class Transfer:
+    def __init__(self, server, port, user, password, file_path):
+        self.client = createSSHClient(server, port, user, password)
+        self.ssh = createSSHClient(server, port, user, password)
+        self.scp = SCPClient(self.ssh.get_transport())
+        self.file_path = file_path
+
+    def scp_get(self):
+        self.scp.get()
+        return
+
+    def scp_put(self):
+        self.scp.put(self.file_path, configuration['RASPBERRY_PI']['DESTINATION_PATH'])
+        return
+
+
+def createSSHClient(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
--- main.py
+++ main.py
@@ -1,15 +1,63 @@
 from automate import automate
+from file import file_transfer
 import time
+import yaml
+
+stream = open("config.yaml", 'r')
+configuration = yaml.load(stream)
+
+server = configuration['RASPBERRY_PI']['IP']
+port = configuration['RASPBERRY_PI']['PORT']
+file = configuration['HOST']['SOURCE_PATH']
+user = configuration['RASPBERRY_PI']['USER']
+password = configuration['RASPBERRY_PI']['PASSWORD']
+
 
 if __name__ == "__main__":
+    temp = True
+    print("Select the following operation:")
     automation = automate.automation()
-    automation.create_automation('automations/test.csv')
-    print("Beginning Playback in 3 sec...")
-    time.sleep(3)
-    automation.automate('automations/test.csv')
+
+    while temp:
+        print("1: Record Automation\n"
+              "2: Run Automation Loop\n"
+              "3: Transfer Test File (Debug Tool)\n"
+              "4: Exit\n\n")
+        user_input = int(input())
+        if user_input == 1:
+            automation.create_automation(
+                configuration['AUTOMATIONS']['DESTINATION_PATH'])
+        elif user_input == 2:
+            automation.automate(
+                instruction_path=configuration['AUTOMATIONS']['DESTINATION_PATH'])
+        elif user_input == 3:
+            file_transfer = file_transfer.Transfer(server=server,
+                                                   port=port,
+                                                   file_path=file,
+                                                   user=user,
+                                                   password=password)
+            file_transfer.scp_put()
+        elif user_input == 4:
+            exit(0)
+        else:
+            print("Invalid choice. Please choose again")
+
+    # automation = automate.automation()
+    # automation.create_automation('automations/test.csv')
+    # print("Beginning Playback in 3 sec...")
+    # time.sleep(3)
+    # automation.automate('automations/test.csv')
 
 
-    # mouse_pos.start_listen()
+
+
+
+
+
+
+
+
+
 
 
 
