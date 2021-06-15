32866e52e37aad8d356646b9dbc1bdb057c9b461
dev <dev@jacksonmed.org>
2021-06-15 13:01:34 -0700

    Updated test_parser.py tests for better code coverage

--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -27,5 +27,27 @@ class Test(TestCase):
         os.remove(save_path)
         assert(isFile)
 
+    def test_extract_sensor_dataframe(self):
+        # Load the correct data df
+        load_path = "./test_files/test_df_correct.csv"
+        correct_df = pd.read_csv(load_path, index_col=0)
+        correct_data_df = pd.DataFrame(ast.literal_eval(correct_df["data"][0]))
+
+        # Load test df and test extract_sensor_dataframe function
+        # txt file first
+        path = "test_files/temp.txt"
+        df = parser.get_latest_frame(path)
+        data_txt_df = parser.extract_sensor_dataframe(df)
+
+        assert_frame_equal(correct_data_df, data_txt_df)
+
+        # csv file
+        path = "test_files/test_df_correct.csv"
+        df = pd.read_csv(path, index_col=0)
+        data_csv_df = parser.extract_sensor_dataframe(df)
+
+        assert_frame_equal(correct_data_df, data_csv_df)
+
+
 
 
